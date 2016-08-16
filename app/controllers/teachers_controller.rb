c5346d7c4985d2d1ababd89bc9f6ba83f6c10adc
cjbuckets <cjbuckets@icloud.com>
2016-08-15 20:10:55 -0500

    added actions to teachers controller

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,2 +1,31 @@
 class TeachersController < ApplicationController
+  def index
+    @teachers = Teacher.all
+  end
+
+  def new
+    @teacher = Teacher.new
+  end
+
+  def create
+    @teacher = Teacher.new(
+    name: params[:name],
+    bio: params[:bio],
+    certifications: params[:certifications]
+    )
+  end
+
+  def edit
+    @teacher = Teacher.find_by(id: params[:id])
+  end
+
+  def update
+    @teacher = Teacher.find_by(id: params[:id])
+  end
+
+  def destroy
+    @teacher = Teacher.find_by(id: params[:id])
+     @teacher.destroy
+  end
+
 end
