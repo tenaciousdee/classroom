da3b0a8aaf669abea92d8bbad65587688351e698
mjonesy <jonesy0007@gmail.com>
2016-08-29 18:48:02 -0500

    added grade and teacher_id validation tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,6 @@ class Student < ActiveRecord::Base
   has_many :images
 
   validates :name, presence: true
+  validates :grade, presence: true
+  validates :teacher_id, presence: true
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -3,3 +3,11 @@ require "rails_helper"
 RSpec.describe Student, "validations" do
   it { is_expected.to validate_presence_of(:name) }
 end
+
+RSpec.describe Student, "validations" do
+  it { is_expected.to validate_presence_of(:grade) }
+end
+
+RSpec.describe Student, "validations" do
+  it { is_expected.to validate_presence_of(:teacher_id) }
+end
